c22eec474811fc9dead930b3d702a1ee71ad7fa1
Javiooli <javipedragosa@gmail.com>
2026-01-22 18:53:30 +0100

    First commit: uv, django, app...

---
+++ cuina/app_cuina/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppCuinaConfig(AppConfig):
+    name = 'app_cuina'
---
+++ cuina/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cuina!")
+
+
+if __name__ == "__main__":
+    main()
