6f34b4a3cbf99b3e5adc07d101f67b9bb4d7f07c
Javiooli <javipedragosa@gmail.com>
2026-01-22 19:51:21 +0100

    Adapted home and views to add home.html

--- cuina/home/urls.py
+++ cuina/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import preview_base
+from .views import preview_base, home
 
 urlpatterns = [
     path("preview/", preview_base, name="preview_base"),
+    path("home/", home, name="home")
 ]
--- cuina/home/views.py
+++ cuina/home/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def preview_base(request):
     return render(request, "home/preview.html")
+
+def home(request):
+    return render(request, "home/home.html")
