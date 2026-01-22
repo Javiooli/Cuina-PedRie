2199f4896b48f36473ee28aeef076be87b8d23cd
Javiooli <javipedragosa@gmail.com>
2026-01-22 19:39:09 +0100

    added apps to settings.py

--- cuina/cuina/settings.py
+++ cuina/cuina/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'faqs',
+    'users',
+    'recipes',
+    'enter_recipes'
 ]
 
 MIDDLEWARE = [
--- cuina/home/views.py
+++ cuina/home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def preview_base(request):
-    return render(request, "base.html")
+    return render(request, "home/preview.html")
